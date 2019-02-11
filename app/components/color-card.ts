6e28920a8127cad3f85ce90e195b2f9b83cc6d7c
Peter Banka <pbanka@fastly.com>
2019-02-10 17:19:28 -0800

    ✨ Pick white or black text based on brightness of selected color

--- app/components/color-card.ts
+++ app/components/color-card.ts
@@ -9,14 +9,39 @@ import { computed } from "@ember-decorators/object";
 import hbs from "htmlbars-inline-precompile";
 import Component from "@ember/component";
 
+const TEST = false;
+
+function brightness(color: string): number {
+  // parse card.color into three variables
+  const redness = (parseInt(color.slice(1, 3), 16) + 1) / 256;
+  const blueness = (parseInt(color.slice(3, 5), 16) + 1) / 256;
+  const greenness = (parseInt(color.slice(5, 7), 16) + 1) / 256;
+  // A bunch of color theory here: different colors don't contribute equally to brightness
+  const redMultiplier = 77 / 255; // because science
+  const blueMultiplier = 150 / 255;
+  const greenMultiplier = 28 / 255;
+  return (
+    redMultiplier * redness +
+    blueMultiplier * blueness +
+    greenMultiplier * greenness
+  );
+}
+
+function toGrayscale(color: string): string {
+  return `hsl(0, 0%, ${brightness(color) * 100}%)`;
+}
+
 export default class ColorCard extends Component {
   // Computed properties
   // ---------------------------------------------------------------------------
   @computed("card.color")
   @attribute
   get style() {
-    // Make the decision if this is a "light" or a "dark" color
-    const color = "white";
+    const color = brightness(this.card.color) > 0.5 ? "black" : "white";
+    if (TEST) {
+      const backgroundColor = toGrayscale(this.card.color);
+      return `background-color: ${backgroundColor}; color: ${color};`;
+    }
     return `background-color: ${this.card.color}; color: ${color};`;
   }
 
