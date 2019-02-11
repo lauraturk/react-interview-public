810cd543b763fd96761a513bb3bbf449e33106e3
Peter Banka <pbanka@fastly.com>
2019-02-11 11:26:31 -0800

    🚨 Fix linting errors

--- app/components/ui-form/component.ts
+++ app/components/ui-form/component.ts
@@ -21,9 +21,9 @@ export default class UiForm extends Component {
   // TODO: take out pre-wiring of submit button
   layout = hbs`
     {{yield (hash
-      label=(component 'ui-form/label')
-      input=(component 'ui-form/input')
-      submit=(component 'ui-button' id="submit" onClick=onSubmit type='submit')
+      label=(component "ui-form/label")
+      input=(component "ui-form/input")
+      submit=(component "ui-button" id="submit" onClick=onSubmit type="submit")
     )}}
   `;
 }
